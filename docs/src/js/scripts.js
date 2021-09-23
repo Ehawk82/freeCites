f5c66285a014d13cc07270179d1516a1ba503f1d
unknown <mrehawk82@gmail.com>
2021-09-23 19:08:58 -0400

    basic layout homepage

--- docs/src/js/scripts.js
+++ docs/src/js/scripts.js
@@ -1,6 +1,31 @@
 const init = () => {
-	console.log("hello JS");
+	launchHomePage();
+},
+launchHomePage = () => {
+	const heading = createEle("h1"),
+		  worker = createEle("div"),
+		  hirer = createEle("div"),
+	      container = createEle("div"),
+	      footer = createEle("footer");
+	//
+	heading.innerHTML = "Free Cites";
+	heading.className = "heading w3-center";
+
+	worker.innerHTML = "BUSINESS";
+	worker.className = "worker w3-button w3-padding-16 w3-margin w3-large w3-monarch";
+
+	hirer.innerHTML = "WORKERS";
+	hirer.className = "hirer w3-button w3-padding-16 w3-margin w3-large w3-monarch";
+
+	container.append(worker,hirer);
+	container.className = "container w3-center w3-card-4";
+
+	footer.innerHTML = "Ehawk LLC &copy; 2021 - Powered by w3css";
+	footer.className = "footer w3-center w3-grey";
+
+	body.append(heading,container,footer);
 };
+
 window.onload = () => {
 	init();
 };
